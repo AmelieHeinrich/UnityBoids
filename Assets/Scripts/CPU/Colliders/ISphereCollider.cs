e7b6dbf648ea3e1ce4ffea0c78959d0c0060e562
Coralie Thiery <cocosteph0312@gmail.com>
2025-10-01 12:56:19 +0200

    Update ISphereCollider.cs

--- Assets/Scripts/CPU/Colliders/ISphereCollider.cs
+++ Assets/Scripts/CPU/Colliders/ISphereCollider.cs
@@ -4,21 +4,20 @@ public class ISphereCollider : ICollider
 {
     [SerializeField] public float Radius = 1f;
 
-    // Distance réelle entre la surface de la sphère et le boid
+  
     public override float DistanceWithSphere(Vector3 spherePos, float sphereRadius)
     {
         float centerDistance = Vector3.Distance(this.transform.position, spherePos);
         return centerDistance - (Radius + sphereRadius);
     }
 
-    // Point le plus proche sur la surface de la sphère
     public override Vector3 ClosestPointOnSurface(Vector3 spherePos)
     {
         Vector3 dir = spherePos - this.transform.position;
         if (dir.sqrMagnitude > 1e-6f)
             dir = dir.normalized;
         else
-            dir = Vector3.up; // fallback si exactement au centre
+            dir = Vector3.up; 
 
         return this.transform.position + dir * Radius;
     }
